74828f6e4b800ffae2d9c0b0515708a5bde1928c
tranthanhthangbmt <thangtt@donga.edu.vn>
2025-05-23 20:13:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings import GoogleGenerativeAIEmbeddings
